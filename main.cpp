17b135c0cffb688695f4af2b89eb6f227a23fc52
John Barbero Unenge <john.barbero.unenge@gmail.com>
2014-02-17 23:41:28 +0100

    Wrote stuffs

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #define FAILED_TO_LOAD_IMAGE 1
 #define FAILED_TO_WRITE_IMAGE 2
 
-#define DISTANCE_FIELD_DISTANCE 10
+#define DISTANCE_FIELD_DISTANCE 20
 
 void showHelp(const char *msg)
 {
@@ -22,7 +22,7 @@ void showHelp(const char *msg)
 
 bool checkPixel(unsigned char pixel[4])
 {
-    return pixel[0] && pixel[1] && pixel[2];
+    return pixel[0] || pixel[1] || pixel[2];
 }
 
 /* Assuming 4 channels */
@@ -63,14 +63,50 @@ bool getNeighborOutlineArray(unsigned char *input, const int &w, const int &h, u
     static const uint channels{4};
     size_t arraySize{uint(w) * uint(h) * channels};
 
+    int passEdge{0};
+    bool passToggle = false;
     for (size_t i{0}; i < arraySize; i += channels)
     {
         bool res = checkNeighbors(input, w, h, i, 1);
         bool me = checkPixel(input + i);
-        output[i+0] = res == me ? 0x00 : 0xff;
+        if (res != me && !passToggle)
+        {
+            ++passEdge;
+            passToggle = true;
+        }
+        else if (res == me)
+            passToggle = false;
+
+        output[i+0] = res == me ? 0x00  : 0xff;
         output[i+1] = res == me ? 0x00 : 0xff;
         output[i+2] = res == me ? 0x00 : 0xff;
         output[i+3] = 0xff;
+    }
+}
+
+bool getOr(unsigned char *input1, unsigned char *input2, const int &w, const int &h, unsigned char *output)
+{
+    assert(w >= 0);
+    assert(h >= 0);
+    static const uint channels{4};
+    size_t arraySize{uint(w) * uint(h) * channels};
+    for (size_t i{0}; i < arraySize; ++i)
+        output[i] = input1[i] | input2[i];
+}
+
+void filter(unsigned char *input, const int &w, const int &h, const int &rgba, unsigned char *output)
+{
+    char r{(rgba & 0xff000000) >> 24}, g{(rgba & 0xff0000) >> 16}, b{(rgba & 0xff00) >> 8}, a{(rgba & 0xff)};
+    assert(w >= 0);
+    assert(h >= 0);
+    static const uint channels{4};
+    size_t arraySize{uint(w) * uint(h) * channels};
+    for (size_t i{0}; i < arraySize; i += channels)
+    {
+        output[i] = input[i] & r;
+        output[i+1] = input[i+1] & g;
+        output[i+2] = input[i+2] & b;
+        output[i+3] = input[i+3] & a;
     }
 }
 
@@ -79,21 +115,19 @@ bool getDistanceField(unsigned char *input, const int &w, const int &h, unsigned
     assert(w >= 0);
     assert(h >= 0);
     static const uint channels{4};
-    static char amount = 0x20;
+    static char amount = 0x60;
     size_t arraySize{uint(w) * uint(h) * channels};
 
     for (size_t i = 0; i < arraySize; i += channels)
     {
-        output[i] = 0x00;
-        output[i+1] = 0x00;
-        output[i+2] = 0x00;
-        output[i+3] = 0xff;
+        output[i] = 0xff;
+        output[i+1] = 0xff;
+        output[i+2] = 0xff;
+        output[i+3] = 0x00;
 
         if (checkPixel(input+i))
         {
-            output[i] = 0x80;
-            output[i+1] = 0x80;
-            output[i+2] = 0x80;
+            output[i+3] = 0x80;
         }
         else
         {
@@ -101,9 +135,8 @@ bool getDistanceField(unsigned char *input, const int &w, const int &h, unsigned
                 if (checkNeighbors(input, w, h, i, j))
                 {
                     float ratio{float(DISTANCE_FIELD_DISTANCE - j) / float(DISTANCE_FIELD_DISTANCE)};
-                    output[i] += ratio * amount;
-                    output[i+1] += ratio * amount;
-                    output[i+2] += ratio * amount;
+                    output[i+3] += ratio * amount;
+                    break;
                 }
         }
     }
@@ -157,17 +190,24 @@ int main(int argc, char **argv)
     memset(edgePixels, 0, arrSize);
     getNeighborOutlineArray(pixels, width, heigt, edgePixels);
 
+    unsigned char *redOriginal = new unsigned char[arrSize];
+    unsigned char *fill = new unsigned char[arrSize];
+    memset(fill, 0, arrSize);
+    filter(pixels, width, heigt, 0xff0000ff, redOriginal);
+    getOr(redOriginal, edgePixels, width, heigt, fill);
+
     unsigned char *distanceField = new unsigned char[arrSize];
     memset(distanceField, 0, arrSize);
-    getDistanceField(edgePixels, width, heigt, distanceField);
+    getDistanceField(pixels, width, heigt, distanceField);
 
 
     std::cout << "Done with filter\n";
 
-    int status = stbi_write_png(out_file, width, heigt, channels, distanceField, 0);
+    int s1 = stbi_write_png(std::string("e_" + std::string(out_file)).c_str(), width, heigt, channels, fill, 0);
+    int s2 = stbi_write_png(out_file, width, heigt, channels, distanceField, 0);
     delete edgePixels;
 
-    if (!status)
+    if (!(s1 && s2))
     {
         std::cerr << "Failed to write: " << out_file << "\nTerminating...\n";
         return FAILED_TO_WRITE_IMAGE;
